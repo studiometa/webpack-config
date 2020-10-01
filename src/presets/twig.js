461c15d68ff92f139132f2adfaee8b9c1b3f635b
Titouan Mathis <titouan@studiometa.fr>
2020-10-01 13:05:36 +0200

    Add a `twig` preset

---
+++ src/presets/twig.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('lodash.merge');
+
+module.exports = (options, config) => {
+  const opts = merge(
+    {
+      loaderOptions: { data: {} },
+      pluginOptions: { template: './src/templates/index.twig' },
+    },
+    options
+  );
+
+  const oldWebpackConfig = typeof config.webpack === 'function' ? config.webpack : () => {};
+  config.webpack = (webpackConfig, isDev) => {
+    oldWebpackConfig(webpackConfig, isDev);
+    webpackConfig.module.rules.push({
+      test: /\.twig$/,
+      use: [
+        'raw-loader',
+        {
+          loader: 'twig-html-loader',
+          options: opts.loaderOptions,
+        },
+      ],
+    });
+    webpackConfig.plugins.push(new HtmlWebpackPlugin(opts.pluginOptions));
+  };
+};
