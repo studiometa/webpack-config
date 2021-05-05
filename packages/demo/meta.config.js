228f0b709bf441070709081ad92133233892a6e7
Titouan Mathis <titouan@studiometa.fr>
2021-05-05 10:55:00 +0200

    Add types

--- packages/demo/meta.config.js
+++ packages/demo/meta.config.js
@@ -1,19 +1,3 @@
-const path = require('path');
-
 module.exports = {
-  watch: [
-    [
-      'dist/*.html',
-      (event, file) => {
-        console.log(`The "${event}" event was emitted for the file "${file}".`);
-      },
-    ],
-  ],
-  webpackDev({ mode, devtool }) {
-    console.log('webpackDev', { mode, devtool });
-  },
-  webpackProd({ mode, devtool }) {
-    console.log('webpackProd', { mode, devtool });
-  },
   presets: ['prototyping', 'yaml'],
 };
---
+++ packages/webpack-config/src/index.d.ts
@@ -0,0 +1,91 @@
+import { Configuration as WebpackConfig } from 'webpack';
+import {
+  ServerOptions,
+  Options as BrowsersyncOptions,
+  BrowserSyncInstance,
+} from '@types/browser-sync';
+
+export type Presets = 'prototyping' | 'tailwindcss' | 'twig' | 'yaml';
+
+export interface MetaConfig {
+  src?: string[];
+  dist?: string;
+  public?: string;
+  /**
+   * Analyze the bundle with the WebpackBundleAnalyzer plugin.
+   */
+  analyze?: boolean;
+  /**
+   * Extends the Webpack configuration before merging
+   * with the environment specific configurations.
+   */
+  webpack?: (config: WebpackConfig, isDev: boolean) => void;
+  /**
+   * Extends the development Webpack configuration.
+   */
+  webpackDev?: (config: WebpackConfig) => void;
+  /**
+   * Extends the production Webpack configuration.
+   */
+  webpackProd?: (config: WebpackConfig) => void;
+  /**
+   * Configure the `sass-loader` options.
+   * @link https://github.com/webpack-contrib/sass-loader#sassoptions
+   */
+  sassOptions?: Object;
+  /**
+   * Configure the browserSync server if you do not use a proxy by setting
+   * this property to `true` or a BrowserSync server configuration object.
+   * If the property is a function, it will be used to alter the server
+   * configuraton and instance in proxy mode.
+   * @link https://browsersync.io/docs/options#option-server
+   */
+  server?:
+    | string
+    | boolean
+    | string[]
+    | ServerOptions
+    | ((config: BrowsersyncOptions, bs: BrowserSyncInstance) => void);
+  /**
+   * Watch for file changes in dev mode and:
+   *
+   * - reload the browser
+   * - or execute a callback
+   *
+   * @example
+   * ```js
+   * watch: [
+   *   // Watch for changes on all PHP files and reload the browser
+   *   '*.php',
+   *   // Watch for all events on all HTML files and execute the callback
+   *   [
+   *     '*.html',
+   *     (event, file, bs) => event === 'change' && bs.reload(),
+   *   ],
+   * ],
+   * ```
+   *
+   * @link https://browsersync.io/docs/api#api-watch
+   */
+  watch: Array<
+    | string
+    | [
+        string,
+        (event: string, file: string, bs: BrowserSyncInstance, bsConfig: BrowsersyncOptions) => void
+      ]
+  >;
+  /**
+   * Use presets to apply pre-made configurations.
+   * The following presets are available:
+   *
+   * - `prototyping` ([doc](https://github.com/studiometa/webpack-config#prototyping))
+   * - `tailwindcss` ([doc](https://github.com/studiometa/webpack-config#tailwindcss))
+   * - `yaml` ([doc](https://github.com/studiometa/webpack-config#yaml))
+   * - `twig` ([doc](https://github.com/studiometa/webpack-config#twig))
+   */
+  presets?: Array<Presets | [Presets, any]>;
+}
+
+declare function createConfig(config: MetaConfig): MetaConfig;
+
+export { createConfig };
