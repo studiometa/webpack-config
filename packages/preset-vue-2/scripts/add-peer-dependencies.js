ae22ac438ec8dc40ad1e75517814a25736469e52
Titouan Mathis <titouan@studiometa.fr>
2022-07-28 16:25:21 +0200

    Fix peerDependencies conflicts

---
+++ packages/preset-vue-2/scripts/add-peer-dependencies.js
@@ -0,0 +1,12 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+const dirname = path.dirname(new URL(import.meta.url).pathname);
+const pkgPath = path.resolve(dirname, '../package.json');
+const pkg = JSON.parse(fs.readFileSync(pkgPath));
+
+pkg.peerDependencies = {
+  vue: '^2.6.14',
+};
+
+fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
