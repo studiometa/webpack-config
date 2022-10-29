7c66158804a7959101264b9f44e64ba280338620
Titouan Mathis <titouan@studiometa.fr>
2022-10-29 15:39:24 +0200

    WIP Use mkcert to generate SSL certificates

--- packages/webpack-config/src/utils/get-browsersync.js
+++ packages/webpack-config/src/utils/get-browsersync.js
@@ -1,4 +1,6 @@
-import path from 'path';
+import { execSync, spawnSync } from 'node:child_process';
+import fs from 'node:fs';
+import path from 'node:path';
 import bs from 'browser-sync';
 import chalk from 'chalk';
 
@@ -6,10 +8,52 @@ const instance = bs.create();
 
 let WATCH_HANDLERS_BINDED = false;
 
+/**
+ * Create certificates on the fly.
+ * @returns {{cert?:string,key?:string}}
+ */
+function createCertificates() {
+  try {
+    execSync('which mkcert', { encoding: 'utf-8' });
+  } catch {
+    console.log('mkcert not found, skipping creating SSL certificates.');
+    return { cert: null, key: null };
+  }
+  const cachePath = path.join(
+    path.dirname(process.env.npm_package_json),
+    'node_modules/.cache/@studiometa/webpack-config/certificates/'
+  );
+
+  execSync(`mkdir -p ${cachePath}`);
+
+  const certPath = path.join(cachePath, 'localhost.cert');
+  const keyPath = path.join(cachePath, 'localhost.key');
+
+  if (fs.existsSync(certPath) && fs.existsSync(keyPath)) {
+    return {
+      cert: certPath,
+      key: keyPath,
+    };
+  }
+
+  try {
+    spawnSync(`mkcert`, [`-cert-file`, certPath, '-key-file', keyPath, 'localhost', '127.0.01']);
+  } catch {
+    console.log('could not create SSL certificates, continuing...');
+    return { cert: null, key: null };
+  }
+
+  return {
+    cert: certPath,
+    key: keyPath,
+  };
+}
+
 const getConfig = (metaConfig) => {
   const browserSyncConfig = {
     open: false,
     logPrefix: '',
+    port: 3042,
     logFileChanges: false,
     logLevel: 'silent',
     notify: {
@@ -59,21 +103,13 @@ const getConfig = (metaConfig) => {
     });
   }
 
-  // Enable `https://` with browserSync
-  if (
-    process.env.APP_SSL &&
-    process.env.APP_SSL === 'true' &&
-    process.env.APP_SSL_CERT &&
-    process.env.APP_SSL_KEY
-  ) {
-    if (browserSyncConfig.proxy) {
-      browserSyncConfig.proxy = `https://${browserSyncConfig.proxy}`;
-    }
+  const { cert, key } = createCertificates();
 
-    // @todo try to create certificate with mkcert if available, otherwise simply use `https = true`
+  // Enable `https://` with browserSync
+  if (cert && key) {
     browserSyncConfig.https = {
-      cert: path.resolve(process.env.APP_SSL_CERT),
-      key: path.resolve(process.env.APP_SSL_KEY),
+      cert,
+      key,
     };
   }
 
@@ -84,10 +120,15 @@ const getConfig = (metaConfig) => {
   return browserSyncConfig;
 };
 
+let config;
+
 export default (metaConfig) => ({
   instance,
   get config() {
-    return getConfig(metaConfig);
+    if (!config) {
+      config = getConfig(metaConfig);
+    }
+    return config;
   },
   get getInfo() {
     return () => {
