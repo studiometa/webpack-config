6ce59c447bbcbac081138c35948ae5231083b515
Titouan Mathis <titouan@studiometa.fr>
2022-11-25 08:43:40 +0100

    Update demo

--- packages/demo/meta.config.js
+++ packages/demo/meta.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
     (isDev) => (isDev ? null : eslint({ fix: false })),
     (isDev) => (isDev ? null : stylelint({ fix: false })),
     prototyping({ ts: true }),
-    yaml(),
     vue(),
     https(),
     {
@@ -21,6 +20,6 @@ export default defineConfig({
   // Exclude the `test.scss` file from the merge
   mergeCSS: /^(?!.*css\/test\.scss).*$/,
   webpack(config) {
-    // config.cache = false;
+    config.cache = false;
   },
 });
