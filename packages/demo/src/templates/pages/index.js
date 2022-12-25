4a656eef59b4cfa939d8e84e399f79444758877b
Titouan Mathis <titouan@studiometa.fr>
2022-12-25 21:07:16 +0100

    Update demo

--- packages/demo/src/templates/pages/index.js
+++ packages/demo/src/templates/pages/index.js
@@ -1,6 +1,9 @@
 export async function data({ pages }) {
   console.log(pages('/notes/**/!(index)*.html'));
   return {
+    head: {
+      title: 'home',
+    },
     foo: ['foo', 'biz'],
     fn() {
       return 'this is value returned by a function';
