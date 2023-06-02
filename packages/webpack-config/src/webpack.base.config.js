23c79208ab8a245b60d1a797f796c9a6b92f02be
Titouan Mathis <titouan@studiometa.fr>
2023-06-02 17:06:06 +0200

    Fix env variable not being expanded

--- packages/webpack-config/src/webpack.base.config.js
+++ packages/webpack-config/src/webpack.base.config.js
@@ -1,4 +1,5 @@
 import path from 'node:path';
+import 'dotenv-expand/config.js';
 import WebpackBar from 'webpackbar';
 import * as glob from 'glob';
 import RemoveEmptyScriptsPlugin from 'webpack-remove-empty-scripts';
@@ -7,14 +8,11 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import BundleAnalyzerPluginImport from 'webpack-bundle-analyzer';
 import TerserPlugin from 'terser-webpack-plugin';
 import commonDir from 'common-dir';
-import dotenv from 'dotenv';
 import WebpackAssetsManifest from 'webpack-assets-manifest';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 
 const { DefinePlugin } = webpack;
 const { BundleAnalyzerPlugin } = BundleAnalyzerPluginImport;
-
-dotenv.config();
 
 const LEADING_SLASH_REGEXT = /^\//;
 
