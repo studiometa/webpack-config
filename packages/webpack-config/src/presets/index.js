5f4709cf45a4e2b36f33f38f88efb09b2bc09d3a
Titouan Mathis <titouan@studiometa.fr>
2023-07-25 11:59:00 +0200

    Remove a deprecated export

--- packages/webpack-config/src/presets/index.js
+++ packages/webpack-config/src/presets/index.js
@@ -2,7 +2,5 @@ export { default as eslint } from './eslint.js';
 export { default as https } from './https.js';
 export { default as stylelint } from './stylelint.js';
 export { default as tailwindcss } from './tailwindcss.js';
-// @todo remove v5
-export { default as withContentHash } from './hash.js';
 export { default as hash } from './hash.js';
 export { default as yaml } from './yaml.js';
