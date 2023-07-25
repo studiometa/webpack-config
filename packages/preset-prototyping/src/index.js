02a14a33a9a6ed301ece3bcfce1b7a191e89318f
Titouan Mathis <titouan@studiometa.fr>
2023-07-25 11:58:44 +0200

    Move the twig preset inside the prototyping package

--- packages/preset-prototyping/src/index.js
+++ packages/preset-prototyping/src/index.js
@@ -7,9 +7,12 @@ import * as glob from 'glob';
 import merge from 'lodash.merge';
 import { minimatch } from 'minimatch';
 import { collect } from 'collect.js';
-import { twig as twigPreset, tailwindcss as tailwindcssPreset, yaml as yamlPreset, hash } from '@studiometa/webpack-config/presets';
+import { tailwindcss as tailwindcssPreset, yaml as yamlPreset, hash } from '@studiometa/webpack-config/presets';
 import markdown from '@studiometa/webpack-config-preset-markdown';
+import twigPreset from './presets/twig.js';
 import Html from './utils/Html.js';
+
+export { twigPreset as twig };
 
 const dirname = path.dirname(new URL(import.meta.url).pathname);
 
--- packages/webpack-config/src/presets/index.js
+++ packages/webpack-config/src/presets/index.js
@@ -2,7 +2,6 @@ export { default as eslint } from './eslint.js';
 export { default as https } from './https.js';
 export { default as stylelint } from './stylelint.js';
 export { default as tailwindcss } from './tailwindcss.js';
-export { default as twig } from './twig.js';
 // @todo remove v5
 export { default as withContentHash } from './hash.js';
 export { default as hash } from './hash.js';
