5c5d41a73c651c3af87b1379c11d31f54136ee0b
Titouan Mathis <titouan@studiometa.fr>
2023-07-25 17:01:51 +0200

    Improve error logs

--- packages/webpack-config/src/build.js
+++ packages/webpack-config/src/build.js
@@ -16,7 +16,7 @@ export default async (options = {}) => {
 
   webpack(webpackConfig, (err, stats) => {
     if (err) {
-      console.error(err.message);
+      console.error(err);
       process.exit(1);
       return;
     }
