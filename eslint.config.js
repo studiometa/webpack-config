c64223f284fbb03ee7a2e28ac830ef42b97db66b
Titouan Mathis <titouan@studiometa.fr>
2024-04-15 15:58:53 +0200

    Improve ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,13 @@
 import { js, prettier } from '@studiometa/eslint-config';
+import { globals } from '@studiometa/eslint-config/utils';
 
-export default [...js, ...prettier];
+export default [
+  ...js,
+  ...prettier,
+  {
+    files: ['packages/preset-*/**/*', 'packages/webpack-config/**/*'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+];
